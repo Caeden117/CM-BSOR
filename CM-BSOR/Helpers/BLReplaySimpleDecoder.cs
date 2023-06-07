d83761cc9a6176fa5fb5e29657178e1b88d14f24
Caeden117 <caeden.s@outlook.com>
2023-06-06 23:16:21 -0700

    document the decoder + get plugin to load

--- CM-BSOR/Helpers/BLReplaySimpleDecoder.cs
+++ CM-BSOR/Helpers/BLReplaySimpleDecoder.cs
@@ -10,6 +10,12 @@ namespace CM_BSOR.Helpers
     // Custom implementation because, as of the time of coding (2023/06/06), BeatLeader's C# implementation is unlicensed.
     public static class BLReplaySimpleDecoder
     {
+        /// <summary>
+        /// Attempts to decode a byte <paramref name="buffer"/>, which ideally represents a valid BeatLeader replay.
+        /// </summary>
+        /// <param name="buffer">Buffer of bytes, ideally representing an encoded replay.</param>
+        /// <param name="replay">If the decode operation was successful, <paramref name="replay"/> is assigned the decoded BeatLeader replay.</param>
+        /// <returns>Whether or not the operation was successful.</returns>
         public static bool TryDecode(byte[] buffer, out SimpleReplay replay)
         {
             replay = null!;
@@ -25,6 +31,11 @@ namespace CM_BSOR.Helpers
             }
         }
 
+        /// <summary>
+        /// Decodes a byte <paramref name="buffer"/> into a BeatLeader replay.
+        /// </summary>
+        /// <param name="buffer">Byte array</param>
+        /// <returns><see cref="null"/> if the BeatLeader replay is invalid, a <see cref="SimpleReplay"/> otherwise.</returns>
         public static SimpleReplay? Decode(byte[] buffer)
         {
             var head = 0;
@@ -40,8 +51,15 @@ namespace CM_BSOR.Helpers
             };
         }
 
+        /// <summary>
+        /// Decodes the first block of a BeatLeader replay: the Metadata block.
+        /// Contains metadata on the game, player, and song.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded <see cref="SimpleReplay.ReplayMetadata"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static SimpleReplay.ReplayMetadata DecodeMetadata(byte[] buffer, ref int head)
+        private static SimpleReplay.ReplayMetadata DecodeMetadata(byte[] buffer, ref int head)
         {
             DecodeByte(buffer, ref head); // Struct type (always metadata)
 
@@ -78,8 +96,14 @@ namespace CM_BSOR.Helpers
             };
         }
 
+        /// <summary>
+        /// Decodes the second block of a BeatLeader replay: HMD and Controller keyframes.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded list of <see cref="SimpleReplay.Frame"/>s.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static List<SimpleReplay.Frame> DecodeFrames(byte[] buffer, ref int head)
+        private static List<SimpleReplay.Frame> DecodeFrames(byte[] buffer, ref int head)
         {
             DecodeByte(buffer, ref head); // Struct type (always frame list)
 
@@ -92,6 +116,12 @@ namespace CM_BSOR.Helpers
             return frames;
         }
 
+        /// <summary>
+        /// Decodes a single frame of keyframe data.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded <see cref="SimpleReplay.Frame"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static SimpleReplay.Frame DecodeFrame(byte[] buffer, ref int head)
             => new()
@@ -103,18 +133,42 @@ namespace CM_BSOR.Helpers
                 RightHand = DecodePose(buffer, ref head),
             };
 
+        /// <summary>
+        /// Decodes a Unity <see cref="Pose"/>, which contains a position and rotation component.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded <see cref="Pose"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Pose DecodePose(byte[] buffer, ref int head)
             => new(DecodeVector3(buffer, ref head), DecodeQuarternion(buffer, ref head));
 
+        /// <summary>
+        /// Decodes a singular <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded <see cref="Vector3"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Vector3 DecodeVector3(byte[] buffer, ref int head)
             => new(DecodeFloat(buffer, ref head), DecodeFloat(buffer, ref head), DecodeFloat(buffer, ref head));
 
+        /// <summary>
+        /// Decodes a singular <see cref="Quaternion"/>.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>A decoded <see cref="Quaternion"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Quaternion DecodeQuarternion(byte[] buffer, ref int head)
             => new(DecodeFloat(buffer, ref head), DecodeFloat(buffer, ref head), DecodeFloat(buffer, ref head), DecodeFloat(buffer, ref head));
 
+        /// <summary>
+        /// Decodes the Player name from a BeatLeader replay.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>Decoded player name</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         // What is special with this method?
         private static string DecodePlayerName(byte[] buffer, ref int head)
@@ -149,6 +203,12 @@ namespace CM_BSOR.Helpers
             return @string;
         }
 
+        /// <summary>
+        /// Decodes a singular <see cref="string"/>.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>Decoded <see cref="string"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static string DecodeString(byte[] buffer, ref int head)
         {
@@ -159,6 +219,12 @@ namespace CM_BSOR.Helpers
             return result;
         }
 
+        /// <summary>
+        /// Decodes a singular <see cref="int"/>.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>Decoded <see cref="int"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static int DecodeInt(byte[] buffer, ref int head)
         {
@@ -167,6 +233,12 @@ namespace CM_BSOR.Helpers
             return result;
         }
 
+        /// <summary>
+        /// Decodes a singular <see cref="float"/>.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>Decoded <see cref="float"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static float DecodeFloat(byte[] buffer, ref int head)
         {
@@ -175,10 +247,22 @@ namespace CM_BSOR.Helpers
             return result;
         }
 
+        /// <summary>
+        /// Returns the byte at the read head.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns>Current byte.</returns>
         // haha
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static byte DecodeByte(byte[] buffer, ref int head) => buffer[head++];
 
+        /// <summary>
+        /// Returns <see cref="true"/> if the byte at the read head is non-zero, <see cref="false"/> otherwise.
+        /// </summary>
+        /// <param name="buffer">Encoded BeatLeader replay.</param>
+        /// <param name="head">Read head, which automatically gets incremented.</param>
+        /// <returns><see cref="true"/> if the byte at the read head is non-zero, <see cref="false"/> otherwise.</returns>
         // hahahaha
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool DecodeBool(byte[] buffer, ref int head) => buffer[head++] > 0;
